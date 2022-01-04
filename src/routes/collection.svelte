ed719c2629655c03ce77da89cec7c9e410af08b1
Sanqui <me@sanqui.net>
2022-01-04 11:21:37 +0100

    Fix favicon and photos under collection page

--- src/routes/collection.svelte
+++ src/routes/collection.svelte
@@ -5,7 +5,7 @@
 
     <h3>Psané materiály</h3>
 
-    <img src="photos/pocitac_pritel_cloveka.jpg" alt="Fotka časopisů Počítač přítel člověka">
+    <img src="/photos/pocitac_pritel_cloveka.jpg" alt="Fotka časopisů Počítač přítel člověka">
 
     <p>Literatura je ověřený zdroj předávání informací po generace. V rámci naší sbírky se soustřeďujeme na knihy související s herní historií u nás či ve světě. Stejně tak důležité jsou dobové časopisy, které umožňují jedinečný vhled do historického kontextu her a techniky. V současnosti naše sbírka herních časopisů pokrývá přes 1500 čísel více jak 50 různých herních magazínů z Česka a Slovenska. Nechybí zde ani malonákladové a samizdatové produkce. V poslední řadě se jedná o jedinečné materiály od herních vývojářů samotných, jako náčrtky z koncepčních fází.</p>
 
@@ -13,13 +13,13 @@
 
     <h3>Hry</h3>
 
-    <img src="photos/tajemstvi_osliho_ostrova.jpg" alt="Fotka krabicové hry Tajemství oslího ostrova, včetně diskety">
+    <img src="/photos/tajemstvi_osliho_ostrova.jpg" alt="Fotka krabicové hry Tajemství oslího ostrova, včetně diskety">
 
     <p>Samozřejmě nemohou chybět hry samotné, kterých máme ve sbírce více jak 1000, a to jak od českých a slovenských tvůrců, tak zahraniční v lokalizované distribuci. Sbíráme hry na původních nosičích a všechna média zálohujeme, neboť např. diskety a optické disky již v dnešní době podléhají skáze a ztrácejí data (tzv. bit rot). Součástí herní prezentace jsou i manuály a krabice. Neopomenutelnou stránkou herní historie jsou i nelicencované kopie zahraničních her, zejména z doby před pádem železné opony.</p>
 
     <h3>Stroje</h3>
 
-    <img src="photos/tesla_xd_8001.jpg" alt="Stroj TV Hra XD 8001 od Tesly včetně analogových ovladačů na Pong">
+    <img src="/photos/tesla_xd_8001.jpg" alt="Stroj TV Hra XD 8001 od Tesly včetně analogových ovladačů na Pong">
 
     <p>Všechny hry jsou navrženy pro technologii své doby. Pokrok ovšem nelze zastavit, a tak jsou časem stroje nahrazeny. Podpora se časem vytratí a staré hry nelze na moderních zařízeních jednoduše přehrát. A i pokud to možné je, jedná se o odlišný zážitek. Staré stroje chátrají a jejich správa vyžaduje určité know-how. V naší sbírce najdete přes 100 strojů, včetně těch československé výroby. Kde je to možné zakomponováváme zajímavé a vzácné periferie i moderní doplňky.</p>
 
