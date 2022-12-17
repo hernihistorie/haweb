9d08792fecca8258877001cca803118c6a9c343e
Brambora2022 <115284790+Brambora2022@users.noreply.github.com>
2022-12-17 20:15:17 +0100

    Update collection.svelte

--- src/routes/collection.svelte
+++ src/routes/collection.svelte
@@ -1,27 +1,27 @@
 <article class="thin">
     <h2>Sbírka</h2>
 
-    <p>Videohry bereme jako umění a součást lidské kultury, která si zaslouží uchování pro další generace. Proto budujeme obsáhlou sbírku herních a počítačových materiálů, a to předně těch v souvislosti s Českem a Slovenskem. Zejména se zabýváme shromažďováním psaných materiálů, her na původních nosičích, a dobových strojů. Všechny předměty v naší sbírce inventarizujeme, třídíme, a bezpečně uchováváme.</p>
+    <p>Videohry bereme jako umění a součást lidské kultury, která si zaslouží zachování pro další generace. Proto budujeme obsáhlou sbírku herních a počítačových materiálů, a to předně těch z Česka a Slovenska. Zejména se zabýváme shromažďováním psaných materiálů, her na původních nosičích, a dobových strojů. Všechny předměty v naší sbírce inventarizujeme, třídíme, a bezpečně uchováváme.</p>
 
     <h3>Psané materiály</h3>
 
     <img src="/photos/pocitac_pritel_cloveka.jpg" alt="Fotka časopisů Počítač přítel člověka">
 
-    <p>Literatura je ověřený zdroj předávání informací po generace. V rámci naší sbírky se soustřeďujeme na knihy související s herní historií u nás či ve světě. Stejně tak důležité jsou dobové časopisy, které umožňují jedinečný vhled do historického kontextu her a techniky. V současnosti naše sbírka herních časopisů pokrývá přes 1500 čísel více jak 50 různých herních magazínů z Česka a Slovenska. Nechybí zde ani malonákladové a samizdatové produkce. V poslední řadě se jedná o jedinečné materiály od herních vývojářů samotných, jako náčrtky z koncepčních fází.</p>
+    <p>Literatura je ověřený zdroj předávání informací po generace. V rámci naší sbírky se soustředíme na knihy související s herní historií u nás či ve světě. Stejně tak důležité jsou dobové časopisy, které umožňují jedinečný vhled do historického kontextu her a techniky. V současnosti naše sbírka herních časopisů pokrývá přes 1500 čísel více jak 50 různých herních magazínů z Česka a Slovenska. Nechybí zde ani malonákladové a samizdatové produkce. V poslední řadě se jedná o jedinečné materiály od samotných herních vývojářů, jako náčrtky z koncepčních fází.</p>
 
-    <p>Psané materiály bezpečně uchováváme a soustavně pracujeme na digitalizaci a zpřístupnění.</p>
+    <p>Psané materiály bezpečně uchováváme a soustavně pracujeme na jejich digitalizaci a zpřístupnění.</p>
 
     <h3>Hry</h3>
 
     <img src="/photos/tajemstvi_osliho_ostrova.jpg" alt="Fotka krabicové hry Tajemství oslího ostrova, včetně diskety">
 
-    <p>Samozřejmě nemohou chybět hry samotné, kterých máme ve sbírce více jak 1000, a to jak od českých a slovenských tvůrců, tak zahraniční v lokalizované distribuci. Sbíráme hry na původních nosičích a všechna média zálohujeme, neboť např. diskety a optické disky již v dnešní době podléhají zkáze a ztrácejí data (tzv. bit rot). Součástí herní prezentace jsou i manuály a krabice. Neopomenutelnou stránkou herní historie jsou i nelicencované kopie zahraničních her, zejména z doby před pádem železné opony.</p>
+    <p>Samozřejmě nemohou chybět hry samotné, kterých máme ve sbírce více jak 1000. Jde o videohry jak od českých a slovenských tvůrců, tak o zahraniční v lokalizovanou distribuci. Sbíráme hry na původních nosičích a všechna média zálohujeme, neboť např. diskety a optické disky mohou podléhat zkáze a ztrácet data (tzv. bit rot). Součástí herní prezentace jsou i manuály a krabice. Neopomenutelnou stránkou herní kultury jsou i nelicencované kopie zahraničních her, zejména z doby před pádem železné opony.</p>
 
     <h3>Stroje</h3>
 
     <img src="/photos/tesla_xd_8001.jpg" alt="Stroj TV Hra XD 8001 od Tesly včetně analogových ovladačů na Pong">
 
-    <p>Všechny hry jsou navrženy pro technologii své doby. Pokrok ovšem nelze zastavit, a tak jsou časem stroje nahrazeny. Podpora se časem vytratí a staré hry nelze na moderních zařízeních jednoduše přehrát. A i pokud to možné je, jedná se o odlišný zážitek. Staré stroje chátrají a jejich správa vyžaduje určité know-how. V naší sbírce najdete přes 100 strojů, včetně těch československé výroby. Kde je to možné zakomponováváme zajímavé a vzácné periferie i moderní doplňky.</p>
+    <p>Všechny hry jsou navrženy pro technologii své doby. Pokrok ovšem nelze zastavit, a tak jsou časem stroje nahrazeny. Podpora se časem vytratí a staré hry nelze na moderních zařízeních jednoduše přehrát. A i pokud to možné je, jedná se o odlišný zážitek. Staré stroje chátrají a jejich správa vyžaduje určité know-how. V naší sbírce najdete přes 100 strojů, včetně těch československé výroby. Také zakomponováváme zajímavé a vzácné periferie i moderní doplňky.</p>
 
     <hr>
 
