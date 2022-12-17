add266db9fb33b38bc4a7fbbc57ee9be6727e6d1
Brambora2022 <115284790+Brambora2022@users.noreply.github.com>
2022-12-17 20:56:20 +0100

    Update mission.svelte

--- src/routes/mission.svelte
+++ src/routes/mission.svelte
@@ -1,15 +1,15 @@
 <article class="thin">
     <h2>Mise a cíle</h2>
     <p>
-        Interaktivní zábava je pořád velmi mladý obor. Můžeme sledovat, jak se každým dnem prohlubuje naše poznání herní historie a je potřeba, aby začaly vznikat organizované snahy. Rozhodli jsme se, že Česká a Slovenská Republika potřebuje reprezentaci v meznárodních snahách, na kterých se často podílíme. A s tím jsme se s nadšením pustili do projektu, který se bude aktivně snažit zachránit co nejvíce z počátků videoher.
+        Interaktivní zábava je pořád velmi mladý obor. Můžeme sledovat, jak se každým dnem prohlubuje naše poznání herní historie a je potřeba, aby začaly vznikat organizované snahy. Věříme, že Česká a Slovenská Republika si zaslouží reprezentaci v mezinárodních snahách, na kterých se často podílíme. Toto nadšení nás vedlo ke spuštění projektu, který se bude snažit zachránit co nejvíce z počátků videoherní kultury u nás.
     </p>
 
     <p>
-        Časem bude potřeba, aby vznikla oficiální instituce zastřešující naší kulturní stopu ve hrách. Jak bude vypadat je otázkou, ale do té doby se budeme snažit prošlapat jí stezku. Veškeré materiály a informace schraňujeme kvůli tomu, aby byly přístupné dalším snahám v oboru. Náš záběr je velký, krom fyzické i digitální archivace je potřeba mluvit s autory, získávat vývojářské materiály, budovat kontakty s podobnými organizacemi i zajišťovat, aby tato práce byla jednodušší pro nově vznikající hry.
+        Časem bude potřeba, aby vznikla oficiální instituce zastřešující naší kulturní stopu ve hrách. Jak bude vypadat je otázkou, ale do té doby se budeme snažit prošlapat jí cestu. Veškeré materiály a informace schraňujeme kvůli tomu, aby byly přístupné dalším snahám v oboru. Náš záběr je velký, krom fyzické i digitální archivace je potřeba mluvit s autory, získávat vývojářské materiály, budovat kontakty s podobnými organizacemi i zajišťovat, aby tato práce byla jednodušší pro nově vznikající hry.
     </p>
 
     <p>
-        Je jisté, že je to běh na dlouhou trať, ale vidíme v něm smysl. Je vidět, že zájem o hry stále roste a získávají na společenské prestiži. Je nám tedy ctí být u tohoto zrodu nového mediálního formátu. Mnoho z nás se hrám věnuje většinu svého života a jsme připraveni věnovat značné usílí tomuto neziskovému projektu. Krom komplexního archivu také zajišťujeme popularizaci historických her a zpřístupňujeme je v autentické podobě v rámci <a href="https://retroherna.org">RetroHerny</a>. Děkujeme za Vaší podporu.
+        Je jisté, že je to běh na dlouhou trať, ale vidíme v něm smysl. Je vidět, že zájem o hry stále roste a že videohry nabývají na společenské prestiži. Je nám tedy ctí být u tohoto zrodu nového mediálního formátu. Mnoho z nás se hrám věnuje většinu svého života a jsme připraveni věnovat značné usílí tomuto neziskovému projektu. Krom komplexního archivu také zajišťujeme popularizaci historických her a zpřístupňujeme je v autentické podobě v rámci <a href="https://retroherna.org">RetroHerny</a>. Děkujeme za Vaší podporu.
     </p>
 
     <img src="/photos/elektronika.jpg" alt="Fotka časopisů Elektronika">
