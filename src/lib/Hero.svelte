468ed837adcb45753c8f15b1325b61ff651cdf60
Sanqui <me@sanqui.net>
2023-06-17 21:42:37 +0200

    Fix typo

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -3,7 +3,7 @@
 	var facts = [
 		[14, "Členů spolku", "Organization members"],
 		[7, "Let snahy", "Years of effort"],
-		[50, "Let historie", "Years of histoy"],
+		[50, "Let historie", "Years of history"],
 		[2800, "Tiskovin a příloh", "Printed materials"],
 		[145, "Místních her", "Local games"],
 		[7.5, "Terabytů dat", "Terabytes of data"],
