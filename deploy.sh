c49ecbebe804ae16007c4a5107096745f32fb072
Sanqui <me@sanqui.net>
2023-10-02 18:43:36 +0200

    Fix building static site and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 npm run build
 scp -r build sanqui.net:tmp_build
-ssh sanqui.net "cp -r tmp_build/* /var/www/haweb/"
+ssh -t sanqui.net "\
+    sudo rm -rf tmp_build/build_ && \
+    mv tmp_build/build tmp_build/build_ && \
+    sudo chown -R www-data. tmp_build/build_ && \
+    sudo mv /var/www/haweb/ /var/www/haweb_old/ 2>/dev/null && \
+    sudo cp -r tmp_build/build_ /var/www/haweb/"
