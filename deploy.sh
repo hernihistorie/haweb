4d4096ea5d1d13c7b8b013ecf3f8c5d989848628
Sanqui <me@sanqui.net>
2023-10-04 23:03:18 +0200

    Hopefully final fix to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ ssh -t sanqui.net "\
     sudo rm -rf tmp_build/build_ && \
     mv tmp_build/build tmp_build/build_ && \
     sudo chown -R www-data. tmp_build/build_ && \
+    sudo rm -rf /var/www/haweb_old && \
     sudo mv /var/www/haweb/ /var/www/haweb_old/ 2>/dev/null && \
     sudo cp -r tmp_build/build_ /var/www/haweb/"
