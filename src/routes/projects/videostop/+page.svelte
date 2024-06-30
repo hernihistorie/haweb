dc45b8732354409dcf34daa8ade001e6c8099edd
Sanqui <me@sanqui.net>
2024-06-30 17:02:24 +0200

    Add new page for project Videostop
    Source for the contents:
    https://docs.google.com/document/d/1WNaftBUdWc1aQ7whJaJtgCPp5Y4CcF5SfQwI6cu6FMg/edit

---
+++ src/routes/projects/videostop/+page.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Meta from "$lib/Meta.svelte";
+</script>
+
+<Meta title="Projekt Videostop" />
+
+<article class="thin">
+    <h2>Projekt Videostop</h2>
+    <div>
+        <p><strong>Název projektu</strong>: Videostop
+        <p><strong>Jména zpracovatelů</strong>: Rudolf Jan Suchý, Karel Ondráček, Vojtěch Straka, Roman Bórik
+        <p><strong>Rok dokončení projektu</strong>: 2024
+
+        <p>V případě otázek či nejasností se nám ozvěte na info@herniarchiv.cz.
+            
+    </div>
+
+    <h3>Úvod</h3>
+    <p>
+        <em>Sem později přijde úvod.</em>
+    </p>
+
+    <h3>Návody</h3>
+    <h4>Návod na spuštění hry Videostop</h4>
+    <!-- TODO prosím dej tam odkaz přímo na ten soubor -->
+    <!-- TODO nahradit "zde"-->
+    <p>
+        Pokud si chcete hru z pořadu <em>Videostop</em> jen zahrát a chcete se vyhnout složitému nastavováním, tak doporučujeme hru stáhnout z <a href="https://cs.speccy.cz/index.php">cs.speccy</a>. Zde kromě originální verze naleznete i verzi upravenou, kterou lze nahrát samu o sobě a také je hratelná jen za pomocí klávesnice. Tato upravená verze je pojmenovaná „Videostop_v1_fixed.tap.” V menu této verze se pohybujete za pomocí čísel a hru ovládáte tlačítkem „7”. Pokud byste si naopak chtěli zahrát hru s ovladačem/joystickem tak spusťte verzi s názvem  „Videostop_v1_fixed_kemp.tap.” I zde menu ovládáte za pomocí čísel ale hru zde ovládáte pomocí Kempston joysticku. (Autor upraveného programu: Roman Bórik)
+    </p>
+    <p>
+        Pro spuštění originální hry z pořadu <em>Videostop</em> budete potřebovat samozřejmě ZX Spectrum (nebo kompatibilní), Kempston interface s ovladačem/joystickem a Basicový interpreter <em>YS MegaBasic</em> (možné stáhnout <a href="https://spectrumcomputing.co.uk/entry/8997/ZX-Spectrum/YS_MegaBasic">zde</a>). Pokud si chcete zahrát hru v emulovaném prostředí, tak budete potřebovat emulátor jenž podporuje i emulaci Kempston Joysticku (např. emulátor <em>Spectaculator</em>) a zmíněný <em>YS MegaBasic</em>. Nakonec budete potřebovat samotnou hru, kterou je možné stáhnout na cs.speccy a nebo z našeho inventáře (viz. Předměty).
+    </p>
+
+    <p>
+        <strong>Nahrávání hry:</strong>
+    </p>
+    <ol>
+        <li>Spusťte počítač/emulátor. V případě, že hrajete hru na původním hardwaru, tak předtím zapojte periferie a naopak poklud používáte emulátor, tak zapněte ovládání pomocí Kempston joystick.
+        </li><li>Po spuštění nahrajte program YS MegaBasic za pomocí příkazu LOAD “”.
+        </li><li>Po nahrání YS MegaBasic stiskněte jakékoliv tlačítko kromě písmena “m”.
+        </li><li>Nyní v YS MegaBasic nahrajte program Videostop za pomocí příkazu LOAD “”.
+        </li><li>Po nahrání hry by se vám mělo zobrazit menu hry. Vyberte pomocí čísel jednu z možností – pro samotnou hru vyberte ZAVERECNE LOSOVANI.
+        </li><li>Nyní by se vám měla zobrazit samotná hra. Ovládáte ji pomocí joysticku zapojeného do Kempston interface. Vzhledem k tomu, že hra se ovládal jen pomocí jednoho velkého tlačítka, tak by měla jakýkoliv input vždy interpretovat, jako zmáčknutí tohoto tlačítka.
+    </ol>
+
+    <h3>Předměty</h3>
+    <!-- TODO fotky predmetu -->
+     <ul>
+        <li>Uživatelská kazeta s programem „Videostop" (Fuji)   https://inventory.retroherna.org/asset/7859
+        <li>Uživatelská kazeta s programem „Videostop" (Magna)	https://inventory.retroherna.org/asset/7860
+    </ul>
+
+    <h3>Rozhovory</h3>
+    <p><em>Sem později přijdou jednotlivé rozhovory.</em>
+    <h3>Články</h3>
+    <p><em>Sem přijdou případné články.</em>
+
+</article>
