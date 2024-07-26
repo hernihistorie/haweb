d692171221d6265c8a8989c631a0396ed4aaaf20
Brambora2022 <115284790+Brambora2022@users.noreply.github.com>
2024-07-26 15:14:53 +0200

    Add fourth asset and pictures to "Project Videostop"

---
+++ src/routes/assets/asset_08077.ts
@@ -0,0 +1,10 @@
+import type { AssetData } from "$src/types";
+
+export const data: AssetData = {
+    name: 'Návrh pořadu „Videostop"',
+    inventory_url: "https://inventory.herniarchiv.cz/asset/8077",
+    description: 'Původní návrh pořadu „Videostop" s ručními poznámky, popisující změny v konceptu pořadu a posudkem zaměstnanců ČST. Zapůjčeno na oskenování Vladimírem Bezděkem (autorem návrhu).',
+    picture: {
+        url: "/assets/asset_8077.jpg",
+    },
+}
