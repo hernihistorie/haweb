1d8d8613a083d6089bc32ada7037cc33bdfcdc9c
Brambora2022 <sucharmail@gmail.com>
2024-12-16 20:28:08 +0100

    Add bio, fix typos, and add link to "das ADLERNEST" game to the interview with Martin Kadlecek

--- src/routes/interviews/martin-kadlcik/interview.ts
+++ src/routes/interviews/martin-kadlcik/interview.ts
@@ -5,17 +5,17 @@ import type { InterviewData, Person } from "$src/types";
 export const MK: Person = {
     name: "Martin Kadlčík",
     shortname: "MK",
-    birth_year: "1872",
-    birth_place: "Uherské hradiště",
+    birth_year: "1972",
+    birth_place: "Uherské Hradiště",
     gender: "M",
     color: "darkblue",
-    bio: ""
+    bio: 'Martin Kadlčík - narozen roku 1972 v Uherském Hradišti - je tvůrce drobné textovky z 80. let jménem <a href="https://inventory.herniarchiv.cz/asset/8411-kazeta-das-adlernest"><em>das ADLERNEST<em></a>, kterou jsme tento rok zdigitalizovali. Mimo svých osobních projektů po revoluci také pracoval na několika nevydaných titulech v Ilusion Softworks a v současnosti pracuje na volné noze pro různá herní studia a v e-shopu sintech.'
 }
 
 export const data: InterviewData = {
     slug: "martin-kadlcik",
     lang: "cs",
-    title: "Vyplněný dotazník: Kadlčík, Martin",
+    title: "Dotazník: Martin Kadlčík",
     narrator: MK,
     interview: {
         type: "questionnaire",
