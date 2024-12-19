5d6f87db73bfadeffab06305188189cc8e458094
Brambora2022 <sucharmail@gmail.com>
2024-12-19 02:38:47 +0100

    Fix  the wrong number of blogposts on the mainpage

--- src/routes/blog/BlogBoxes.svelte
+++ src/routes/blog/BlogBoxes.svelte
@@ -27,6 +27,8 @@
     Povedlo se nám dát dohromady funkční sestavu na zálohování DAT kazet.
     </BlogBox>
 
+    {#if all}
+
     <BlogBox href="/blog/30-bytefest-2024"
     title="Na ByteFestu jsme zachránili hru na ZX Spectrum (mimojiné)"
     >
@@ -38,8 +40,6 @@
     >
     V rámci činnosti Cítovského klubu začal v 90. letech vznikat komerčně distribuovaný výukový software, který právě v současnosti digitalizujeme. Druhá várka tohoto softwaru, kterou se nám povedlo zachránit, jsou 2 výukové hry na 8-bit Atari distribuovaných na kazetách.
     </BlogBox>
-
-    {#if all}
 
     <BlogBox href="/blog/28-citov-kazety-spectrum"
     title="PROJEKT CÍTOV - Kazety s výukovými hrami na ZX Spectrum"
