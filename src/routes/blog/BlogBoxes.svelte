0f8249932b1003753d63927bb033f82620d1cfdd
Brambora2022 <sucharmail@gmail.com>
2025-04-16 00:57:40 +0200

    Fix typo in URL of a blogpost

--- src/routes/blog/BlogBoxes.svelte
+++ src/routes/blog/BlogBoxes.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div class="blogboxes">
-    <BlogBox href="/blog/43-bulanci-dotaznik"
+    <BlogBox href="/blog/44-bulanci-dotaznik"
     title="Projekt Fanouškovské aktivity kolem hry Bulánci - DOTAZNÍK (testovací kolo)"
     >
     Mnozí z vás jistě vyrůstali na prvních Buláncích - na hře, která několik let v kuse okupovala mnoho školních, firemních a domácích počítačů a která byla hlavní náplní každé hodiny, která probíhala v nějaké počítačové učebně. Narozdíl od jiných významných her československého kánonu si Bulánky moc lidí nepamatuje pro příběh, ani pro hlášky ale právě pro sdílený zážitek z jejich hraní.
