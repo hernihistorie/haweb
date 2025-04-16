769f5536660d46204c6190efef23ce42f700b8b3
Brambora2022 <sucharmail@gmail.com>
2025-04-16 19:59:53 +0200

    Fix picture in Bulanci glogpost and Change name of a blogpost

--- src/routes/blog/BlogBoxes.svelte
+++ src/routes/blog/BlogBoxes.svelte
@@ -10,7 +10,7 @@
 
 <div class="blogboxes">
     <BlogBox href="/blog/44-bulanci-dotaznik"
-    title="Projekt Fanouškovské aktivity kolem hry Bulánci - DOTAZNÍK (testovací kolo)"
+    title="Projekt Fanouškovské aktivity kolem hry Bulánci - DOTAZNÍK"
     >
     Mnozí z vás jistě vyrůstali na prvních Buláncích - na hře, která několik let v kuse okupovala mnoho školních, firemních a domácích počítačů a která byla hlavní náplní každé hodiny, která probíhala v nějaké počítačové učebně. Narozdíl od jiných významných her československého kánonu si Bulánky moc lidí nepamatuje pro příběh, ani pro hlášky ale právě pro sdílený zážitek z jejich hraní.
     </BlogBox>
