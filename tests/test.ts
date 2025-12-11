0bb7bd8569fe6c058d8056a13cb06d3f6e56c720
Sanqui <me@sanqui.net>
2025-12-11 18:36:18 +0100

    Add test for feed

--- tests/test.ts
+++ tests/test.ts
@@ -5,3 +5,8 @@ test('index page has expected h1', async ({ page }) => {
 	// There's possibly a zero width em dash in "Československý", hence the regex
 	await expect(page.getByRole('heading', { name: /Česko.?slovenský herní archiv/ })).toBeVisible();
 });
+
+test('feed.xml loads without error', async ({ page }) => {
+	const response = await page.goto('/feed.xml');
+	expect(response?.status()).toBe(200);
+});
