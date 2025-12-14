18fcadad1745f203891653f992ff3be36fb9c087
Sanqui <me@sanqui.net>
2025-12-14 20:18:02 +0100

    Recreate index page of magazine database

---
+++ src/lib/components/magazines/MagazinesMenu.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { localizeHref } from "$src/lib/paraglide/runtime";
+	import Loc from "$lib/components/Loc.svelte";
+
+</script>
+
+<ul>
+    <li>
+        <a href={localizeHref("/magazines/")}>
+            <Loc
+                cs="Databáze časopisů"
+                en="Maganizes database"
+            />
+        </a>
+    </li>
+    <li>
+        <a href={localizeHref("/magazines/")}>
+            <Loc    
+                cs="Přehled časopisů"
+                en="foo"
+            />
+        </a>
+    </li>
+    <li>
+        <a href={localizeHref("/magazines/")}>
+            <Loc    
+                cs="Chybí nám"
+                en="foo"
+            />
+        </a>
+    </li>
+</ul>
+
+<style>
+
+	ul {
+		margin: auto;
+		/* margin-right: -27px; */
+        padding: 0;
+		list-style: none;
+		display: flex;
+		justify-content: start;
+		flex-wrap: wrap;
+	}
+
+	li {
+		padding: 0 27px 0 27px;
+		border-left: 1px solid var(--color-secondary);
+    }
+	li:first-child {
+        padding-left: 0;
+		border-left: none;
+	}
+
+	a {
+		color: inherit;
+		text-transform: uppercase;
+		text-decoration: none;
+		white-space: nowrap;
+		user-select: none;
+		border: none;
+		font-size: inherit;
+		padding: 0;
+		margin: 0;	
+		font-weight: normal;
+		font-family: 'Barlow', sans-serif;
+	}
+</style>
---
+++ src/routes/magazines/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Meta from "$src/lib/components/layout/Meta.svelte";
+	import Loc from "$lib/components/Loc.svelte";
+	import { localizeHref } from '$lib/paraglide/runtime';
+	import PageLang from "$src/lib/components/PageLang.svelte";
+	import MagazinesMenu from "$src/lib/components/magazines/MagazinesMenu.svelte";
+</script>
+
+<Meta title={{
+    cs: "Databáze časopisů",
+    en: "Magazines Database"
+}} />
+
+<MagazinesMenu />
+
+<PageLang cs />
+
+<article>
+    <h2>
+        <Loc
+            cs="Vítejte v databázi československých herních časopisů!"
+            en="Welcome to the Czechoslovak Game Magazines Database!"
+        />
+    </h2>
+    <p>
+         Tento projekt vám přináší projekt Herní archiv spolku <a href={localizeHref('/')}>Herní historie</a>. Cílem projektu je vytvořit knihovnu československých herních časopisů, včetně jejich digitalizace. 
+    </p>
+    <p>
+         V současné chvíli máme přibližne 95% procent všech časopisů, o kterých víme, že v Česku a Slovensku vycházeli a jsme v procesu plnění této databáze. Proto, pokud zde nenacházíte nějaký časopis, zatím nepanikařte - možná jsme ho ještě nestihli do databáze přepsat. 
+    </p>
+    <p>
+         Nicméně jsme si jisti, že nemáme úplně vše. Proto zde existuje stránka <a href="">chybí nám</a>, na které lze najít, která čísla kterého časopisu nám zatím chybí. Pokud nějaký takový časopis máte, prosím neváhejte nás kontaktovat na emailu <a href="info@herniarchiv.cz">info@herniarchiv.cz</a>. Budeme velmi vděční, když nám pomůžete zachraňovat československou herní historii! 
+    </p>
+    <img src="https://casopisy.herniarchiv.cz/static/magdb/library.jpg" alt="Fotografie knihovny s magazíny Level">
+</article>
