cff1e01c1b6102eed4cbb3b84f32e4cab1d41fa4
Brambora2022 <sucharmail@gmail.com>
2025-12-22 21:38:35 +0100

    Add to interview page section with public and unpublished interviews

--- src/routes/interviews/+page.svelte
+++ src/routes/interviews/+page.svelte
@@ -17,7 +17,8 @@
 	import Loc from "$lib/components/Loc.svelte";
 
 
-    let interviews: InterviewData[] = [havelka_data, kadlcik_data, vodak_data, kopecky_data, hrda_data, robinett_data, bezdek_data, hurnikova_data, tomas_bartovsky, rudolf_pecinovsky, stefan_kratochvil];
+    let interviews_public: InterviewData[] = [havelka_data, kadlcik_data, vodak_data];
+    let interviews_unpublished: InterviewData[] = [robinett_data, kopecky_data, hrda_data, hurnikova_data, bezdek_data, tomas_bartovsky, rudolf_pecinovsky, stefan_kratochvil];
 </script>
 
 <Meta title={{
@@ -34,11 +35,26 @@
     </h2>
     <p>
         <Loc
-        cs="Sbírka rozhovorů je primárně složená z interview užívající metodu orální historie s představiteli české a slovenské herní kultury. Tyto rozhovory se zaměřují především na široké životopisné vyprávění, během kterého je snaha dát narátorstvu co nejvíce prostoru pro jejich autentický projev. To znamená, že je jejich projev jen minimálně přerušován a veden samotným tazatelem. Tato svědectví rozšiřují poznání o tuzemské videoherní scéně způsobem, který nemohou jiné historické prameny nabídnout. Tato sbírka také obsahuje rozhovory se zahraničními osobnostmi videoherní kultury, více strukturované rozhovory a záznamy přednášek, konferencí, debat a dalších akcí."
+        cs="Sbírka rozhovorů je primárně složená z interview užívající metodu orální historie s představitelstvem české a slovenské herní kultury. Tyto rozhovory se zaměřují především na široké životopisné vyprávění, během kterého je snaha dát narátorstvu co nejvíce prostoru pro jejich autentický projev. To znamená, že je jejich projev jen minimálně přerušován a veden samotným tazatelem. Tato svědectví rozšiřují poznání o tuzemské videoherní scéně způsobem, který nemohou jiné historické prameny nabídnout. Tato sbírka také obsahuje rozhovory se zahraničními osobnostmi videoherní kultury, více strukturované rozhovory a záznamy přednášek, konferencí, debat a dalších akcí."
         en="This collection is mainly composed of oral history interviews with representatives of Czech and Slovak gaming culture. These interviews focus primarily on broad biographical narration, during which the aim is to give as much space as possible to the narrators authentic expression. This means the speech of narrator is not much interrupted or guided by the interviewer. These testimonies expand knowledge about the domestic video game scene in a way that other historical sources cannot offer. Collection also includes interviews with foreign figures from the video game culture, more structured interviews, or recordings of lectures, conferences, debates, and other events. <b>Most interviews are transcribed only into Czech.</b>"
         />
     </p>
-    {#each interviews as data}
+    <hr>
+    <h3>
+        <Loc
+        cs="Veřejné rozhovory"
+        en="Public interviews"/>
+    </h3>
+    {#each interviews_public as data}
+        <InterviewBox {data} />
+    {/each}
+    <hr>
+    <h3>
+        <Loc
+        cs="Nepřepsané a neveřejné rozhovory"
+        en="Untranscribed and unpublished interviews"/>
+        </h3>
+    {#each interviews_unpublished as data}
         <InterviewBox {data} />
     {/each}
 </div>
