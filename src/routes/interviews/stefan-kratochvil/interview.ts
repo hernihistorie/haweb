48cca031fc55bc089eac4a7617b7a3215de4bffb
Sanqui <me@sanqui.net>
2026-01-05 11:07:37 +0100

    Fix grammar, add english

--- src/routes/interviews/stefan-kratochvil/interview.ts
+++ src/routes/interviews/stefan-kratochvil/interview.ts
@@ -18,14 +18,17 @@ export const data: InterviewData = {
         name: "Štefan Kratochvíl",
         birth_year: "1956",
         gender: "M",
-        bio: "Štefan Kratochvíl se narodil roku 1956. Díky jeho zájmu o elektroniku se na začátku 80. let dostal ke Stanici mladých techniků v Podbabě. Později se účastnil i aktivit v rámci 602. ZO Svazarmu, 666. ZO Svazarmu, nebo Střediska pro mládež a elektrotechniku.",
+        bio: {
+            cs: "Štefan Kratochvíl se narodil roku 1956. Díky svému zájmu o elektroniku se na začátku 80. let dostal ke Stanici mladých techniků v Podbabě. Později se účastnil i aktivit v rámci 602. ZO Svazarmu, 666. ZO Svazarmu, nebo Střediska pro mládež a elektrotechniku.",
+            en: "Štefan Kratochvíl was born in 1956. Thanks to his interest in electronics, he got involved with the Young Technicians Station in Podbaba in the early 1980s. Later, he also participated in activities within the 602nd and 666th ZO Svazarm, as well as the Center for Youth and Electrical Engineering."
+        },
         photo: {
             url: "/interviews/stefan-kratochvil.jpg",
         }
     },
     interview: {
         date: Temporal.PlainDate.from("2025-12-17"),
-        place: "Kancelář narátora, Praha",
+        place: {cs: "Kancelář narátora, Praha", en: "Narrator's office, Prague"},
         languages: [czech],
         interviewers: [RS, AT],
         length: "1:43:58",
