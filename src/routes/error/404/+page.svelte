6fb4868f17d4650304cf4a1bfeb9a48b2ee8eccd
Sanqui <me@sanqui.net>
2026-02-12 17:33:08 +0100

    Add 404 page

---
+++ src/routes/error/404/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import BulletPoint from "$src/lib/components/BulletPoint.svelte";
+	import Meta from "$src/lib/components/layout/Meta.svelte";
+</script>
+
+<Meta title="404 Page not found" />
+
+<article class="thin">
+    <h2>
+        Error 404
+    </h2>
+    <p><strong>Stránka nenalezena <BulletPoint /> Page not found</strong></p>
+
+    <p>
+        Tato stránka neexistuje.  Pokud si myslíte, že by měla, prosíme <a href="/contact/">kontaktujte nás</a>.  Můžete také vyzkoušet <a href="/search/">vyhledávat</a>, nebo se můžete vrátit na <a href="/">domovskou stránku</a>.
+    </p>
+    <p>
+        This page does not exist. If you think it should, please <a href="/en/contact/">contact us</a>. You can also try <a href="/en/search/">searching</a>, or you can return to the <a href="/en/">home page</a>.
+    </p>
+</article>
