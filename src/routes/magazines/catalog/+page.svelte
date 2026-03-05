27cfaf9d20fcef1dd2a0a9178950c59794650947
Sanqui <me@sanqui.net>
2026-03-05 17:20:09 +0100

    MagDB: Add notice about incomplete catalog

--- src/routes/magazines/catalog/+page.svelte
+++ src/routes/magazines/catalog/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+    import IconInfo from '@lucide/svelte/icons/info';
 	import Box from '$src/lib/components/Box.svelte';
 	import Meta from '$src/lib/components/layout/Meta.svelte';
-import Loc from '$src/lib/components/Loc.svelte';
+    import Loc from '$src/lib/components/Loc.svelte';
 	import MagazineBlock from '$src/lib/components/magazines/MagazineBlock.svelte';
 	import MagazineLogo from '$src/lib/components/magazines/MagazineLogo.svelte';
 	import MagazinesMenu from '$src/lib/components/magazines/MagazinesMenu.svelte';
@@ -30,6 +31,30 @@ import Loc from '$src/lib/components/Loc.svelte';
         <MagazineBlock {magazine} />
     {/each}
 </div>
+
+<section>
+    <p>
+        <IconInfo />
+        <strong>
+            <Loc
+                cs="Katalog časopisů zatím není kompletní."
+                en="This magazine catalog is not yet complete."
+            />
+        </strong>
+    </p>
+    <p>
+        <Loc>
+            {#snippet cs()}
+                Časopisy většinou přidáváme soustavně s uveřejněním jejich skenů.<br>
+                V případě že chcete pomoct databázi rozšířit, můžete se <a href={localizeHref('/contact')}>nám ozvat</a>.
+            {/snippet}
+            {#snippet en()}
+                We usually add magazines continuously with the publication of their scans.<br>
+                If you want to help expand the database, you can <a href={localizeHref('/contact')}>contact us</a>.
+            {/snippet}
+        </Loc>
+    </p>
+</section>
 
 <style>
     .magazines {
