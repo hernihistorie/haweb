a6009ec9de9b482e69f730f821241e293b9d56bd
Sanqui <me@sanqui.net>
2026-03-16 21:01:10 +0100

    Translate blog post 84

--- src/routes/blog/84-zpravodaje-hifi-klub/+page.svelte
+++ src/routes/blog/84-zpravodaje-hifi-klub/+page.svelte
@@ -5,6 +5,7 @@
     import blogPost from "./blog_post";
 	import Figure from "$src/lib/components/Figure.svelte";
 	import Arrow from "$src/lib/components/Arrow.svelte";
+	import { localizeHref } from "$src/lib/paraglide/runtime";
 </script>
 
 <PageLang cs en />
@@ -12,9 +13,14 @@
 <BlogPostPage post={blogPost}>
     <Loc>
         {#snippet cs()}
-            <p>Již nějakou dobu nezpět jsme k digitalizaci dostali zapůjčené zpravodaje pražského HiFi klubu z let 1972-1997. Přestože s hrami příliš nesouvisí, je to i tak zajímavý a vzácný materiál. Díky zaměření na spotřební elektroniku obsahuje občas zajímavé postřehy o výpočetní technice a zároveň hrál klub malou roli v prehistorii televizních her u nás. HiFi klub Svazarmu Praha dokonce vyráběl audiovizuální techniku a vydával hudbu pro prodej běžným spotřebitelům, což je mezi organizacemi podobného zaměření unikát. A právě z tohoto klubu se také oddělila legendární 602ka se zaměřením na počítače.</p>
+            <p>Již nějakou dobu nezpět jsme k digitalizaci dostali zapůjčené <strong>zpravodaje pražského HiFi klubu z let 1972-1997</strong>. Přestože s hrami příliš nesouvisí, je to i tak zajímavý a vzácný materiál. Díky zaměření na spotřební elektroniku obsahuje občas zajímavé postřehy o výpočetní technice a zároveň hrál klub malou roli v prehistorii televizních her u nás. HiFi klub Svazarmu Praha dokonce vyráběl audiovizuální techniku a vydával hudbu pro prodej běžným spotřebitelům, což je mezi organizacemi podobného zaměření unikát. A právě z tohoto klubu se také oddělila legendární 602ka se zaměřením na počítače.</p>
             <Arrow href="https://archive.org/details/zpravodaje">Prohlédnout skeny Zpravodajů HiFi klubu</Arrow>
-            <p>Přestože je to obsáhlá sbírka, má také mnohé mezery, takže pokud máte tyto nebo podobné klubové zpravodaje okolo elektroniky, určitě se ozvěte! Právě i k 602ce nám chybí velké množství zpravodajů.</p>
+            <p>Přestože je to obsáhlá sbírka, má také mnohé mezery, takže pokud máte tyto nebo podobné klubové zpravodaje okolo elektroniky, určitě se <a href={localizeHref('/contact')}>ozvěte</a>! Právě i k 602ce nám chybí velké množství zpravodajů.</p>
+        {/snippet}
+        {#snippet en()}
+            <p>Some time ago, we received on loan for digitization <strong>journals of the Prague HiFi club from 1972-1997</strong>. Although not directly related to games, it is still interesting and rare material. Thanks to its focus on consumer electronics, it occasionally contains interesting observations about computing technology, and the club also played a small role in the prehistory of video games in Czechoslovakia. The HiFi club of Svazarm Prague even manufactured audiovisual equipment and released music for sale to regular consumers, which is unique among organizations with a similar focus. And it was from this club that the legendary group 602, focused on computers and later called Software602, split off.</p>
+            <Arrow href="https://archive.org/details/zpravodaje">Browse scans of the HiFi club journal</Arrow>
+            <p>Although this is an extensive collection, it also has many gaps, so if you have these or similar club newsletters about electronics, please <a href={localizeHref('/contact')}>get in touch</a>! We are also missing a large number of newsletters for the 602 group specifically.</p>
         {/snippet}
     </Loc>
     <Figure src="/photos/blog-posts/zpravodaje_hifi.jpg" />
--- src/routes/blog/84-zpravodaje-hifi-klub/blog_post.ts
+++ src/routes/blog/84-zpravodaje-hifi-klub/blog_post.ts
@@ -9,14 +9,18 @@ const blogPost: BlogPost = {
     slug: 'zpravodaje-hifi-klub',
     image: '/photos/blog-posts/zpravodaje_hifi.jpg',
     title: {
-        cs: 'Zdigitalizovali jsme zpravodaje pražského HiFi klubu'
+        cs: 'Zdigitalizovali jsme zpravodaje pražského HiFi klubu',
+        en: 'We digitized journals of the Prague HiFi club'
         },
     date: new Temporal.PlainDate(2026, 3, 16),
-    // english_translation_date: new Temporal.PlainDate(2026, 1, 22),
+    english_translation_date: new Temporal.PlainDate(2026, 3, 16),
     author: authors.HerniHistorie,
     description_html: {
         cs: `
             Nějakou dobu nezpět jsme k digitalizaci dostali zapůjčené zpravodaje pražského HiFi klubu z let 1972-1997. Díky zaměření na spotřební elektroniku obsahuje občas zajímavé postřehy o výpočetní technice a zároveň hrál klub malou roli v prehistorii televizních her u nás.
+        `,
+        en: `
+            Some time ago, we received on loan for digitization the journals of the Prague HiFi club from 1972-1997. Thanks to its focus on consumer electronics, it occasionally contains interesting observations about computing technology, and the club also played a small role in the prehistory of video games in Czechoslovakia.
         `
     }
 };
