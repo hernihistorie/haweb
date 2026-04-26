ccdacdb4233d39b465c45e3a5d2753385c4f9bea
Sanqui <me@sanqui.net>
2026-04-26 14:52:11 +0200

    Translate volunteer blog post into English

--- src/data/blog/86-stante-se-dobrovolnikem-spolku-herni-historie/blog_post.ts
+++ src/data/blog/86-stante-se-dobrovolnikem-spolku-herni-historie/blog_post.ts
@@ -10,10 +10,14 @@ const blogPost: BlogPost = {
     image: '/photos/blog-posts/stante-se-dobrovolnikem-spolku-herni-historie/stante-se-dobrovolnikem-spolku-herni-historie.jpg',
     title: {
         cs: "Staňte se dobrovolníkem spolku Herní historie!",
+        en: "Become a volunteer for the Herní historie association!"
     },
     date: new Temporal.PlainDateTime(2026, 4, 29, 8, 25),
     author: authors.HerniHistorie,
-    description_html: "V našem spolku už 11 let pracujeme na popularizaci a zachování tuzemské i zahraniční herní historie. Naše činnost je z podstaty nezisková, a proto se dlouhodobě spoléháme na pomoc dobrovolníků, kteří se aktivitám spolku věnují ve svém volném čase. A právě teď se k nám můžete připojit i vy!",
+    description_html: {
+        cs: "V našem spolku už 11 let pracujeme na popularizaci a zachování tuzemské i zahraniční herní historie. Naše činnost je z podstaty nezisková, a proto se dlouhodobě spoléháme na pomoc dobrovolníků, kteří se aktivitám spolku věnují ve svém volném čase. A právě teď se k nám můžete připojit i vy!",
+        en: "Within our association, we've been working for 11 years to popularize and preserve both local and global game history. Our activities are principally nonprofit, and that's why in the long run, we're relying on the activities of volunteers, who contribute to the group's activities in their free time. And now's the time when you can join in too!"
+    },
     bufferPostId: '69e95bca37c7cb9a4e08fc71',
 };
 
