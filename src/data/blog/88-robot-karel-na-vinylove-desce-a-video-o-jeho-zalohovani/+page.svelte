359e6177ded84690f42d93a32f2c8c1d70b4cbfb
Sanqui <me@sanqui.net>
2026-05-18 11:07:54 +0200

    Add English translation for Karel LP blog post

--- src/data/blog/88-robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani/+page.svelte
+++ src/data/blog/88-robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani/+page.svelte
@@ -4,16 +4,23 @@
 	import PhotoCredit from "$src/lib/components/PhotoCredit.svelte";
 	import AssetList from "$src/lib/components/asset/AssetList.svelte";
     import Figure from "$src/lib/components/Figure.svelte";
+	import Youtube from "svelte-youtube-embed";
 </script>
 
-<PageLang cs />
+<PageLang cs en />
 
 <section>
     <Loc>
         {#snippet cs()}
             <p>Když Rudolf Jan Suchý a Agáta Trlidová dělali rozhovor s Tomášem Bartovským o Robotu Karlovi, tak od něj dostali i pár dárečků do archivu - specificky dvě vinylové desky a instrukce k jejich používání. Po zevrubné digitalizaci, na které se podílel i Jan Heřman jsme vykřesali několik programů pro ZX Spectrum (včetně nezazálohované verze Karla pro zmíněnou platformu) ale i Karla pro IQ151. O tomto dobrodružství <a href="https://www.youtube.com/watch?v=gccZcTYVXbk">natočil Jan Heřman video</a>, které naleznete na jeho kanále Andrew Tesla. Ve videu je také rozhovor s Rudolfem Janem Suchým, o historii toho, jak a proč se Karel na LPčka vlastně dostal.</p>
         {/snippet}
+        {#snippet en()}
+            <p>
+                When Rudolf Jan Suchý and Agáta Trlidová were making an interview with Tomáš Bartovský about Robot Karel, they also received a few gifts for the archive - specifically two vinyl records and instructions on how to use them. After a thorough digitization process, which also involved Jan Heřman, we managed to resurrect several ZX Spectrum programs (including a previously unpreserved version of Karel for this platform) but also Karel for the Czechoslovak IQ 151 home computer. Jan Heřman recorded <a href="https://www.youtube.com/watch?v=gccZcTYVXbk">a video about this adenture</a>, which you will find on his channel, Andrew Tesla. The video also features an interview with Rudolf Jan Suchý about the history of how and why Karel ended up on vinyl records in the first place.
+            </p>
+        {/snippet}
     </Loc>
+    <Youtube id="gccZcTYVXbk" />
     <AssetList assetTagId={45} withHeading />
     <Figure src="/photos/blog-posts/robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani/robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani.jpg" />
     <PhotoCredit>Rudolf Jan Suchý</PhotoCredit>
--- src/data/blog/88-robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani/blog_post.ts
+++ src/data/blog/88-robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani/blog_post.ts
@@ -10,10 +10,14 @@ const blogPost: BlogPost = {
     image: '/photos/blog-posts/robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani/robot-karel-na-vinylove-desce-a-video-o-jeho-zalohovani.jpg',
     title: {
         cs: "️Robot Karel na vinylové desce a video o jeho zálohování",
+        en: "Robot Karel on a vinyl record and a video about his digitization",
     },
     date: new Temporal.PlainDateTime(2026, 5, 18, 8, 25),
     author: authors.HerniHistorie,
-    description_html: "️Tomáš Bartovský, autor české verze Robota Karla, nám do archivu minulý rok věnoval dvě vinylové desky a instrukce k jejich používání. Po zevrubné digitalizaci, na které se podílel i Jan Heřman se nám z desek podařilo vydolovat několik starých programů. O procesu této digitalizace vzniklo video.",
+    description_html: {
+        cs: "Tomáš Bartovský, autor české verze Robota Karla, nám do archivu minulý rok věnoval dvě vinylové desky a instrukce k jejich používání. Po zevrubné digitalizaci, na které se podílel i Jan Heřman se nám z desek podařilo vydolovat několik starých programů. O procesu této digitalizace vzniklo video.",
+        en: "Tomáš Bartovský, author of the Czech version of Robot Karel, donated two vinyl records with usage instructions to our archive last year. After thorough digitization, which also involved Jan Heřman, we managed to extract several old programs from the records. A video was created about this digitization process.",
+    },
     bufferPostId: '6a07c0a2c8c77dcca90b66b9',
 };
 
