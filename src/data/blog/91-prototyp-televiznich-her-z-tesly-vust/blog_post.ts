22d1961793aea4a50c395122f23b1f9ad20d21b3
Sanqui <me@sanqui.net>
2026-09-26 12:20:34 +0200

    Add English translation for new pong blog post

--- src/data/blog/91-prototyp-televiznich-her-z-tesly-vust/blog_post.ts
+++ src/data/blog/91-prototyp-televiznich-her-z-tesly-vust/blog_post.ts
@@ -2,7 +2,8 @@ import { Temporal } from '@js-temporal/polyfill';
 
 import type { BlogPost } from '$src/types';
 import { authors } from '$src/data/authors';
-import img01 from "./img/01-cela-sestava.jpg";
+import img01 from "./img/01-cela-sestava.jpg?w=1600;800&enhanced";
+
 
 const blogPost: BlogPost = {
     published: true,
@@ -11,17 +12,17 @@ const blogPost: BlogPost = {
     image: img01,
     title: {
         cs: 'Prototyp Televizních her z Tesly VÚST',
-        // en: 'Tesla VÚST'
+        en: 'Television game prototype from Czechoslovak Tesla VÚST'
     },
-    date: new Temporal.PlainDate(2026, 9, 27),
+    date: new Temporal.PlainDateTime(2026, 9, 26, 14, 0),
     // english_translation_date: new Temporal.PlainDate(2026, 9, 27),
     author: authors.JiriBernasek,
     description_html: {
         cs: `
-            TODO TODO TODO TODO 
+             V roce 1977 zažilo Československo příchod atraktivní novinky - takzvaných Televizních her. V tomto článku se zaměříme na přístroj TV HRY od Tesly VÚST, nejpokročilejší dobovou tuzemskou hru a dnes patřící mezi nejstarší dochované. Bez využití mikroprocesoru nebo herního čipu nabízí čtyři hry na motivy pongu.
         `,
         en: `
-            TODO TODO TODO TODO 
+            In 1977, Czechoslovakia saw the arrival of an exciting novelty - the so-called Television games. In this article we focus on the TV HRY machine from Tesla VÚST, the most advanced contemporary local game and today one of the oldest surviving. Without the use of a microprocessor or game chip it offers four pong-like games.
         `
     }
 };
